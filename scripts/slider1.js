7481827b6985eb9fab79d90cc8c6a3fda5918244
Anatoliy Komarov <admin@komaroff.biz>
2021-04-03 09:21:33 +0300

    5

--- scripts/slider1.js
+++ scripts/slider1.js
@@ -1,97 +1,115 @@
-
-let blockId ="slide1";
-let left = document.getElementById('left1');
-let right = document.getElementById('right1');
-// click = document.querySelectorAll('.arrows');
-// slide1.addEventListener('click', (e) => {
-// 	blockId = 'slide1';
-// 	left = document.getElementById(e.target.id);
-// 	right = document.getElementById(e.target.id);
-// });
-// slide2.addEventListener('click', (e) => {
-// 	blockId = 'slide2';
-// 	left = document.getElementById(e.target.id);
-// 	right = document.getElementById(e.target.id);
-// });
+let imgWidth = 388; // width img+padding slider1
+let imgWidth2 = 290; // width img+padding slider2
+let blockId = "slide1";
+let left = document.getElementById("left1");
+let right = document.getElementById("right1");
+const timeout = 500;
+document.querySelector(".carousel").style = "width: var(--box-width)";
+let amountImgForShow = 3;//amount of  img slider1
+document.querySelector(".carousel2").style = "width: var(--box-width)";
+let amountImgForShow2 = 4;// amount of  img slider2
 
 karusel1 = document.getElementById(blockId);
-// ПЕРЕТАСКИВАНИЕ
+// movement
 let xCoordinateMovement = 0;
 let xCoordinateStart = 0;
-karusel1.addEventListener('dragstart', startMovement);
+karusel1.addEventListener("dragstart", startMovement);
 function startMovement(e) {
-	let check = false;
-	xCoordinateStart = e.pageX;
-	karusel1.addEventListener('dragend', (e) => {
-		xCoordinateMovement = e.pageX;
-		if (xCoordinateMovement < xCoordinateStart && !check) {
-			shiftLeft();
-			check = true;
-		}
-		if (xCoordinateMovement > xCoordinateStart && !check) {
-			shiftRight();
-			check = true;
-		}
-	});
+  let check = false;
+  xCoordinateStart = e.pageX;
+  karusel1.addEventListener("dragend", (e) => {
+    xCoordinateMovement = e.pageX;
+    if (xCoordinateMovement < xCoordinateStart && !check) {
+      shiftLeft();
+      check = true;
+    }
+    if (xCoordinateMovement > xCoordinateStart && !check) {
+      shiftRight();
+      check = true;
+    }
+  });
 }
 ////// left & right
-left.addEventListener('click', shiftLeft);
-left.addEventListener('mousedown', function(down) {
-	if (down.button !== 0) return;
-	let interval = setInterval(shiftLeft, 500);
-	left.addEventListener('mouseup', (upe) => {
-		if (upe.button === 0) {
-			clearInterval(interval);
-		}
-	});
+left.addEventListener("click", shiftLeft);
+left.addEventListener("mousedown", function (down) {
+  if (down.button !== 0) return;
+  let interval = setInterval(shiftLeft, timeout);
+  left.addEventListener("mouseup", (upe) => {
+    if (upe.button === 0) {
+      clearInterval(interval);
+    }
+  });
 });
-right.addEventListener('click', shiftRight);
-right.addEventListener('mousedown', function(down) {
-	if (down.button !== 0) return;
-	let interval2 = setInterval(shiftRight, 500);
-	right.addEventListener('mouseup', function(up) {
-		if (up.button === 0) {
-			clearInterval(interval2);
-		}
-	});
+right.addEventListener("click", shiftRight);
+right.addEventListener("mousedown", function (down) {
+  if (down.button !== 0) return;
+  let interval2 = setInterval(shiftRight, timeout);
+  right.addEventListener("mouseup", function (up) {
+    if (up.button === 0) {
+      clearInterval(interval2);
+    }
+  });
 });
 
 //SLIDER
-const imgWidth = 386; // ширина картинки+ отступ
-const amountImgForShow = 3; // сколько картинок показываем
+document.addEventListener("DOMContentLoaded", function (event) {
+  window.onresize = () => {
+    clientWidth = document.documentElement.clientWidth;
+    if (clientWidth <= 1200 && clientWidth > 895) {
+      document.querySelector(".carousel").style = "width: 776px";
+      amountImgForShow = 2;
+      document.querySelector(".carousel2").style = "width: 870px";
+      amountImgForShow2 = 3;
+    } else if (clientWidth <= 895 && clientWidth > 780) {
+      document.querySelector(".carousel2").style = "width: 580px";
+      amountImgForShow2 = 2;
+    } else if (clientWidth <= 780 && clientWidth > 580) {
+      document.querySelector(".carousel").style = "width: 388px";
+      amountImgForShow = 1;
+    } else if (clientWidth <= 580) {
+      document.querySelector(".carousel2").style = "width: 290px";
+      amountImgForShow2 = 1;
+    }
+    if (clientWidth > 1200) {
+      document.querySelector(".carousel").style = "width: var(--box-width)";
+      amountImgForShow = 3;
+      document.querySelector(".carousel2").style = "width: var(--box-width)";
+      amountImgForShow2 = 4;
+    }
+  };
+});
+
 let shiftX = imgWidth; // на сколько сдвинуть по оси x
 let shiftX2 = 0;
 let counter = 0;
 let offset = 0;
-// if(blockId=="slide1"){ images = document.querySelectorAll('#slide1 img');}
-// if(blockId=="slide2"){ images = document.querySelectorAll('#slide2 img');}
-images = document.querySelectorAll(`#${blockId} .product__cart-new`);
+images = document.querySelectorAll(`#${blockId} .product__card-new`);
 let allImgAmaunt = images.length;
 
 function shiftLeft() {
-	if (offset != 0) shiftX += offset;
-	if (counter == allImgAmaunt - amountImgForShow) {
-		shiftX -= offset;
-		counter--;
-		offset = 0;
-	}
-	karusel1.style.transform = 'translateX(' + -shiftX + 'px)';
-	offset = imgWidth;
-	counter++;
+  if (offset != 0) shiftX += offset;
+  if (counter == allImgAmaunt - amountImgForShow) {
+    shiftX -= offset;
+    counter--;
+    offset = 0;
+  }
+  karusel1.style.transform = "translateX(" + -shiftX + "px)";
+  offset = imgWidth;
+  counter++;
 }
 
 function shiftRight() {
-	shiftX2 = -shiftX + imgWidth;
-	shiftX = -shiftX2;
-	if (shiftX == 0 && shiftX2 == 0) {
-		shiftX = imgWidth;
-		shiftX2 = 0;
-		counter = 0;
-		offset = 0;
-	}
-	karusel1.style.transform = 'translateX(' + shiftX2 + 'px)';
-	counter--;
-	if (counter < 0) {
-		counter = 0;
-	}
+  shiftX2 = -shiftX + imgWidth;
+  shiftX = -shiftX2;
+  if (shiftX == 0 && shiftX2 == 0) {
+    shiftX = imgWidth;
+    shiftX2 = 0;
+    counter = 0;
+    offset = 0;
+  }
+  karusel1.style.transform = "translateX(" + shiftX2 + "px)";
+  counter--;
+  if (counter < 0) {
+    counter = 0;
+  }
 }
--- scripts/slider2.js
+++ scripts/slider2.js
@@ -1,96 +1,81 @@
-let blockId2 = 'slide2';
-left2 = document.getElementById('left2');
-right2 = document.getElementById('right2');
-// click = document.querySelectorAll('.arrows');
-// slide1.addEventListener('click', (e) => {
-// 	blockId2 = 'slide1';
-// 	left = document.getElementById(e.target.id);
-// 	right = document.getElementById(e.target.id);
-// });
-// slide2.addEventListener('click', (e) => {
-// 	blockId2 = 'slide2';
-// 	left = document.getElementById(e.target.id);
-// 	right = document.getElementById(e.target.id);
-// });
+let blockId2 = "slide2";
+left2 = document.getElementById("left2");
+right2 = document.getElementById("right2");
 
 karusel2 = document.getElementById(blockId2);
-// ПЕРЕТАСКИВАНИЕ
+// movement
 let xCoordinateMovement2 = 0;
 let xCoordinateStart2 = 0;
-karusel2.addEventListener('dragstart', startMovement2);
+karusel2.addEventListener("dragstart", startMovement2);
 function startMovement2(e) {
-	let check = false;
-	xCoordinateStart2 = e.pageX;
-	karusel2.addEventListener('dragend', (e) => {
-		xCoordinateMovement2 = e.pageX;
-		if (xCoordinateMovement2 < xCoordinateStart2 && !check) {
-			shiftLeft2();
-			check = true;
-		}
-		if (xCoordinateMovement2 > xCoordinateStart2 && !check) {
-			shiftRight2();
-			check = true;
-		}
-	});
+  let check = false;
+  xCoordinateStart2 = e.pageX;
+  karusel2.addEventListener("dragend", (e) => {
+    xCoordinateMovement2 = e.pageX;
+    if (xCoordinateMovement2 < xCoordinateStart2 && !check) {
+      shiftLeft2();
+      check = true;
+    }
+    if (xCoordinateMovement2 > xCoordinateStart2 && !check) {
+      shiftRight2();
+      check = true;
+    }
+  });
 }
 ////// left & right
-left2.addEventListener('click', shiftLeft2);
-left2.addEventListener('mousedown', function(down) {
-	if (down.button !== 0) return;
-	let interval = setInterval(shiftLeft2, 500);
-	left2.addEventListener('mouseup', (upe) => {
-		if (upe.button === 0) {
-			clearInterval(interval);
-		}
-	});
+left2.addEventListener("click", shiftLeft2);
+left2.addEventListener("mousedown", function (down) {
+  if (down.button !== 0) return;
+  let interval = setInterval(shiftLeft2, timeout);
+  left2.addEventListener("mouseup", (upe) => {
+    if (upe.button === 0) {
+      clearInterval(interval);
+    }
+  });
 });
-right2.addEventListener('click', shiftRight2);
-right2.addEventListener('mousedown', function(down) {
-	if (down.button !== 0) return;
-	let interval2 = setInterval(shiftRight2, 500);
-	right2.addEventListener('mouseup', function(up) {
-		if (up.button === 0) {
-			clearInterval(interval2);
-		}
-	});
+right2.addEventListener("click", shiftRight2);
+right2.addEventListener("mousedown", function (down) {
+  if (down.button !== 0) return;
+  let interval2 = setInterval(shiftRight2, timeout);
+  right2.addEventListener("mouseup", function (up) {
+    if (up.button === 0) {
+      clearInterval(interval2);
+    }
+  });
 });
 
 //SLIDER
-const imgWidth2 = 290; // ширина картинки+ отступ
-const amountImgForShow2 = 4; // сколько картинок показываем
 let shiftX_2 = imgWidth2; // на сколько сдвинуть по оси x
 let shiftX_22 = 0;
 let counter2 = 0;
 let offset2 = 0;
-// if(blockId2=="slide1"){ images = document.querySelectorAll('#slide1 img');}
-// if(blockId2=="slide2"){ images = document.querySelectorAll('#slide2 img');}
-images2 = document.querySelectorAll(`#${blockId2} .product__cart`);
+images2 = document.querySelectorAll(`#${blockId2} .product__card`);
 let allImgAmaunt2 = images2.length;
 
 function shiftLeft2() {
-	if (offset2 != 0) shiftX_2 += offset2;
-	if (counter2 == allImgAmaunt2 - amountImgForShow2) {
-		shiftX_2 -= offset2;
-		counter2--;
-		offset2 = 0;
-	}
-	karusel2.style.transform = 'translateX(' + -shiftX_2 + 'px)';
-	offset2 = imgWidth2;
-	counter2++;
+  if (offset2 != 0) shiftX_2 += offset2;
+  if (counter2 == allImgAmaunt2 - amountImgForShow2) {
+    shiftX_2 -= offset2;
+    counter2--;
+    offset2 = 0;
+  }
+  karusel2.style.transform = "translateX(" + -shiftX_2 + "px)";
+  offset2 = imgWidth2;
+  counter2++;
 }
 
 function shiftRight2() {
-	shiftX_22 = -shiftX_2 + imgWidth2;
-	shiftX_2 = -shiftX_22;
-	if (shiftX_2 == 0 && shiftX_22 == 0) {
-		shiftX_2 = imgWidth2;
-		shiftX_22 = 0;
-		counter2 = 0;
-		offset2 = 0;
-	}
-	karusel2.style.transform = 'translateX(' + shiftX_22 + 'px)';
-	counter2--;
-	if (counter2 < 0) {
-		counter2 = 0;
-	}
+  shiftX_22 = -shiftX_2 + imgWidth2;
+  shiftX_2 = -shiftX_22;
+  if (shiftX_2 == 0 && shiftX_22 == 0) {
+    shiftX_2 = imgWidth2;
+    shiftX_22 = 0;
+    counter2 = 0;
+    offset2 = 0;
+  }
+  karusel2.style.transform = "translateX(" + shiftX_22 + "px)";
+  counter2--;
+  if (counter2 < 0) {
+    counter2 = 0;
+  }
 }
