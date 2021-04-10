fca1b215026bbc1ca5a308672f0ea7b55b331d2f
Anatoliy Komarov <admin@komaroff.biz>
2021-04-10 10:29:58 +0300

    1

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -3,7 +3,7 @@ id.addEventListener("click",()=>{
     let menu = document.getElementById("mobile");
     menu.classList.remove("header__megamenu-list");
     menu.classList.add("mobile");
-    document.querySelector(".header__megamenu-internallist").style.cssText="left: 80px;max-width: 210px;";
+    document.querySelector(".header__megamenu-internallist").style.cssText="left: 80px;max-width: 70vw;";
     document.querySelector(".none").classList.remove("none");
 })
 let id_close=document.getElementById("close");
