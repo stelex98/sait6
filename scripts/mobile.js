c7d4f01fe31a6a6e51108bfe2d5fe680470f065f
Anatoliy Komarov <admin@komaroff.biz>
2021-04-10 10:18:00 +0300

    2

---
+++ scripts/mobile.js
@@ -0,0 +1,16 @@
+let id= document.querySelector(".start-mobile-menu");
+id.addEventListener("click",()=>{
+    let menu = document.getElementById("mobile");
+    menu.classList.remove("header__megamenu-list");
+    menu.classList.add("mobile");
+    document.querySelector(".header__megamenu-internallist").style.cssText="left: 80px;max-width: 210px;";
+    document.querySelector(".none").classList.remove("none");
+})
+let id_close=document.getElementById("close");
+id_close.addEventListener("click",()=>{
+    let menu = document.getElementById("mobile");
+    menu.classList.remove("mobile");
+    menu.classList.add("header__megamenu-list");
+    document.querySelector(".header__megamenu-internallist").style.cssText="";
+    document.querySelector(".close-menu").classList.add("none");
+})
